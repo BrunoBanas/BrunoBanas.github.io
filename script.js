67b3de688ac28dce12c5ab3072417e26f09492c1
BrunoBanas <banas.bruno@gmail.com>
2024-10-22 12:31:14 -0400

    subsection scrolling:(

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 // Wait for the document to load before running the script 
 (function ($) {
-  
-  // We use some Javascript and the URL #fragment to hide/show different parts of the page
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
-  $(window).on('load hashchange', function(){
-    
-    // First hide all content regions, then show the content-region specified in the URL hash 
-    // (or if no hash URL is found, default to first menu item)
-    $('.content-region').hide();
-    
-    // Remove any active classes on the main-menu
-    $('.main-menu a').removeClass('active');
-    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    
-    // Now show the region specified in the URL hash
-    $(region).show();
-    
-    // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
 
-    // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
-    // This will extract the region name from URL hash, and then load [region].html into the main #content div
-    // var region = location.hash.toString() || '#first';
-    // $('#content').load(region.slice(1) + '.html')
-    
+  // Hide all content regions except for the one specified in the URL hash
+  $(window).on('load hashchange', function () {
+    $('.content-region').hide(); // Hide all content regions
+    var region = location.hash.toString() || $('.main-menu a:first').attr('href'); // Default to first menu item
+    $(region).show(); // Show the selected region
+
+    // Highlight the menu link associated with this region
+    $('.main-menu a').removeClass('active'); // Remove active classes
+    $('.main-menu a[href="' + region + '"]').addClass('active');
   });
-  
+
+  // Show the corresponding main section when a main menu link is clicked
+  $('.main-menu a.menu-link').on('click', function (event) {
+    event.preventDefault(); // Prevent default anchor click behavior
+    var targetSection = $(this.getAttribute('href'));
+    $('.content-region').hide(); // Hide all sections
+    targetSection.show(); // Show the selected section
+  });
+
+  // Smooth scrolling for subsection links
+  $('a[href^="#research-subsection"], a[href^="#interest-subsection"], a[href^="#dofe-subsection"]').on('click', function (event) {
+    event.preventDefault(); // Prevent default anchor click behavior
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 800); // Smooth scroll duration (800 ms)
+
+      // Ensure the parent section is shown when a subsection is clicked
+      var parentSection = target.closest('.content-region');
+      $('.content-region').hide(); // Hide all sections
+      parentSection.show(); // Show the section of the clicked subsection
+    }
+  });
+
 })(jQuery);
